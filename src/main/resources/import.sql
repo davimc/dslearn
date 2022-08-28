149af52c2bd7a992f735532574b8cf2a563db152
davimc <davimatosc@hotmail.com>
2022-08-28 08:39:49 -0300

    Offer

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,3 +14,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Curso HTML','https://cdn.pixabay.com/photo/2021/03/18/17/09/teacher-6105336_960_720.png','https://mir-s3-cdn-cf.behance.net/project_modules/disp/816f5d7636141.560aef70c492f.jpg');
+
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-07-14T00:00:00Z', TIMESTAMP WITH TIME ZONE '2021-07-14T00:00:00Z',1);
+INSERT INTO tb_offer (edition, start_moment, end_moment,course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2021-04-28T00:00:00Z', TIMESTAMP WITH TIME ZONE '2022-04-28T00:00:00Z',1);
+INSERT INTO tb_offer (edition, start_moment, end_moment,course_id) VALUES ('2.1',TIMESTAMP WITH TIME ZONE '2022-08-21T00:00:00Z', TIMESTAMP WITH TIME ZONE '2023-08-21T00:00:00Z',1);
