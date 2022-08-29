76b2ac34264f37dbab9b0c4559002b3737aaf1db
davimc <davimatosc@hotmail.com>
2022-08-28 23:49:28 -0300

    Enrollment

---
+++ src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -0,0 +1,82 @@
+package com.devsuperior.dslearnbds.entities;
+
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant refundMoment;
+    private boolean avaible;
+    private boolean updateOnly;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean avaible, boolean updateOnly) {
+        this.id.setUser(user);
+        this.id.setOffer(offer);
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.avaible = avaible;
+        this.updateOnly = updateOnly;
+    }
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User user) {
+        this.id.setUser(user);
+    }
+
+    public Offer getOffer() {
+        return id.getOffer();
+    }
+
+    public void setOffer(Offer offer) {
+        this.id.setOffer(offer);
+    }
+
+    public Instant getEnrollMoment() {
+        return enrollMoment;
+    }
+
+    public void setEnrollMoment(Instant enrollMoment) {
+        this.enrollMoment = enrollMoment;
+    }
+
+    public Instant getRefundMoment() {
+        return refundMoment;
+    }
+
+    public void setRefundMoment(Instant refundMoment) {
+        this.refundMoment = refundMoment;
+    }
+
+    public boolean isAvaible() {
+        return avaible;
+    }
+
+    public void setAvaible(boolean avaible) {
+        this.avaible = avaible;
+    }
+
+    public boolean isUpdateOnly() {
+        return updateOnly;
+    }
+
+    public void setUpdateOnly(boolean updateOnly) {
+        this.updateOnly = updateOnly;
+    }
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,9 +15,9 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Curso HTML','https://cdn.pixabay.com/photo/2021/03/18/17/09/teacher-6105336_960_720.png','https://mir-s3-cdn-cf.behance.net/project_modules/disp/816f5d7636141.560aef70c492f.jpg');
 
-INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-07-14T00:00:00Z', TIMESTAMP WITH TIME ZONE '2021-07-14T00:00:00Z',1);
-INSERT INTO tb_offer (edition, start_moment, end_moment,course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2021-04-28T00:00:00Z', TIMESTAMP WITH TIME ZONE '2022-04-28T00:00:00Z',1);
-INSERT INTO tb_offer (edition, start_moment, end_moment,course_id) VALUES ('2.1',TIMESTAMP WITH TIME ZONE '2022-08-21T00:00:00Z', TIMESTAMP WITH TIME ZONE '2023-08-21T00:00:00Z',1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-07-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-07-14T00:00:00Z',1);
+INSERT INTO tb_offer (edition, start_moment, end_moment,course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2021-04-28T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-04-28T00:00:00Z',1);
+INSERT INTO tb_offer (edition, start_moment, end_moment,course_id) VALUES ('2.1',TIMESTAMP WITH TIME ZONE '2022-08-21T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-08-21T00:00:00Z',1);
 
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Primeiro feedback de tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-10T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Segundo feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-12T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
@@ -30,3 +30,6 @@ INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_uri, prerequisite_id, resource_id) VALUES ('Visão geral', 'o que irá aprender', 1, 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/816f5d7636141.560aef70c492f.jpg', null, 1)
 INSERT INTO tb_section (title, description, position, img_uri, prerequisite_id, resource_id) VALUES ('Aula ao vivo', 'tirando dúvidas', 2, 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/816f5d7636141.560aef70c492f.jpg', 1, 1)
 INSERT INTO tb_section (title, description, position, img_uri, prerequisite_id, resource_id) VALUES ('Desafio do capítulo', 'implemente o que foi aprendido', 3, 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/816f5d7636141.560aef70c492f.jpg', 1, 1)
+
+INSERT INTO tb_enrollment (user_id, offer_id,enroll_Moment, refund_Moment, avaible, update_Only) VALUES (1,1,TIMESTAMP WITH TIME ZONE '2020-07-14T03:00:00Z', null, true, false)
+INSERT INTO tb_enrollment (user_id, offer_id,enroll_Moment, refund_Moment, avaible, update_Only) VALUES (2,1,TIMESTAMP WITH TIME ZONE '2020-07-14T03:00:00Z', null, true, false)
