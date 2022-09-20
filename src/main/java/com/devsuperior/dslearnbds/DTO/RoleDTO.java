abd24f5b5a4612620eb19e7423c1f31dc6ff23d8
davimc <davimatosc@hotmail.com>
2022-09-20 12:20:57 -0300

    Cleaning code

--- src/main/java/com/devsuperior/dslearnbds/DTO/RoleDTO.java
+++ src/main/java/com/devsuperior/dslearnbds/DTO/RoleDTO.java
@@ -2,40 +2,19 @@ package com.devsuperior.dslearnbds.DTO;
 
 import com.devsuperior.dslearnbds.entities.Role;
 
-import java.io.Serializable;
-
-public class RoleDTO implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+public class RoleDTO {
     private Long id;
     private String authority;
 
     public RoleDTO() {
     }
 
-    public RoleDTO(Long id, String firstName, String lastName, String email) {
+    public RoleDTO(Long id, String authority) {
         this.id = id;
         this.authority = authority;
     }
-
     public RoleDTO(Role entity) {
         id = entity.getId();
         authority = entity.getAuthority();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAuthority() {
-        return authority;
-    }
-
-    public void setAuthority(String authority) {
-        this.authority = authority;
     }
 }
--- src/main/java/com/devsuperior/dslearnbds/DTO/UserUpdateDTO.java
+++
@@ -1,8 +0,0 @@
-package com.devsuperior.dslearnbds.DTO;
-
-import com.devsuperior.dslearnbds.services.validation.UserUpdateValid;
-
-@UserUpdateValid
-public class UserUpdateDTO extends UserDTO {
-    private static final long serialVersionUID = 1L;
-}
